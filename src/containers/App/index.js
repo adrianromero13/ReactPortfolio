e7a76a9e6d94975f2e1e49701559cf40457ef114
Adrian Romero <radrian031613@gmail.com>
2020-04-09 12:18:34 -0700

    added navbar to components and got it to render through App.js

--- src/containers/App/index.js
+++ src/containers/App/index.js
@@ -1,15 +1,12 @@
 import React from 'react';
-import About from './About';
-import Contact from './Contact';
-import Portfolio from './Portfolio';
-class App extends Component {
-    render() {
-      return (
-        <div className="container">
-          
-        </div>
-      );
-    }
+import About from './../About';
+import Contact from './../Contact';
+import Portfolio from './../Portfolio';
+import Navbar from './../../components/NavBar'
+
+
+function App() {
+    return <Navbar/>
   }
   
   export default App;
