c937554128ff984263913b1cf91ec7bf20bdbbcf
Adrian Romero <radrian031613@gmail.com>
2020-04-17 18:48:45 -0700

    Got path for images working for the About page.

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 
+import GrizleyPeak from './../../assets/Images/GrizleyPeak.jpg';
 import './../../assets/Styles/styles.css';
 
 
@@ -14,13 +15,15 @@ const About = props => (
             </div>
             <div className="row">
                 <div className="col-4 text-light justify-content-end">
-                    <img src="" alt="me out in the woods" className="shadow-lg p-2 mb-5 img-fluid" max-width="100%" height="auto" />
+                    <img src={GrizleyPeak} alt="me out in the woods" className="shadow-lg p-2 mb-5 img-fluid" max-width="100%" height="auto" />
                 </div>
                 <div className="col text-light text-left">
                     <p>
-                        Born and raised in the small and lively city of <strong>Pomona</strong>, <strong>California</strong>, where I grew up with a strong affiliation to music, art, and the life sciences.
-                            Before moving to the Bay Area for bigger, and better opportunnities, Pomona is where I learned to play 5 different instruments, picked up on the culture of brewing beer, and graduated with a Bachelor's of Science in Microbiology.
-                        </p>
+                        Born and raised in the small and lively city of <strong>Pomona</strong>, <strong>California</strong>, <br/>where I grew up with a strong affiliation to music, art, and the life sciences.
+                    </p>
+                    <p> 
+                        Before moving to the Bay Area for bigger, and better opportunnities, Pomona is where I learned to play 5 different instruments, picked up on the culture of brewing beer, and graduated with a Bachelor's of Science in Microbiology.
+                    </p>
                     <p>
                         Now I am living in The Bay, where I enjoy cycling, homebrewing, occasionally visiting new places in the energetic city of San Fransisco.
                         On my down time, I enjoy playing video games, reading medical journals, watching cooking tutorials, youtuber chefs, and my favorite anime T.V shows.
